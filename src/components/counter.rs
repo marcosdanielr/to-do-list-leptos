fa5fee7d0bab5a1927cc1991eae9a7b27f7bd8cb
Marcos Roque <devmarcosdanielr@gmail.com>
2025-03-03 00:03:27 -0300

    feat: add styles

--- src/components/counter.rs
+++ src/components/counter.rs
@@ -5,10 +5,14 @@ pub fn Counter() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
-        <button
-            on:click=move |_| set_count.set(count.get() + 1)
+      <button
+            class:red=move || count.get() % 2 == 1
+            on:click=move |_| {
+                *set_count.write() += 1;
+            }
         >
             "Click me: "
+
             {count}
         </button>
         <p>
